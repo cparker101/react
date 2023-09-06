6a1184a36ca17af9c5d1a3d6eef1d819c3174188
Charlie Parker <cp@charlies-mbp-2.mynetworksettings.com>
2023-09-06 15:04:46 -0400

    Chuck Norris

--- src/contacts.js
+++ src/contacts.js
@@ -15,14 +15,14 @@ const contacts = [
     phone: "+987 654 321",
     email: "jack@nowhere.com",
   },
-  // {
-  //   id: 3,
-  //   name: "Chuck Norris",
-  //   imgURL:
-  //     "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-  //   phone: "+918 372 574",
-  //   email: "gmail@chucknorris.com",
-  // },
+  {
+    id: 3,
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
 ];
 
 export default contacts;
